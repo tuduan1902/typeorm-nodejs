366b511dec05531d29e5965f966c264752a173b8
Tu Duan <uuxinhxinh2701@gmail.com>
2022-09-30 15:05:52 +0700

    Connect database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello');
+import {createConnection} from 'typeorm'
+
+const main = async () => {
+   try{
+      await createConnection({
+         type: "postgres",
+         host: "localhost",
+         port: 5433,
+         username: 'postgres',
+         password: "tptd1234560",
+         database: 'typeorm'
+      });
+      console.log('Connected to Postgres');
+   } catch(error) {
+      console.error(error);
+      throw new Error('Unable to connect to db');
+   }
+}
+main();
